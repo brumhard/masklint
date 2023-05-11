e34e0d95027a5d5841bad0cb65aac9cd16d0859d
Tobias Brumhard <code@brumhard.com>
2023-05-11 18:21:05 +0200

    feat: add CLI interface with run and dump

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,51 @@
 use anyhow::Result;
+use clap::{command, Parser, Subcommand};
 use mask_parser::maskfile::Script;
 use std::{
-    fmt::format,
     fs::{self, File},
     io::Write,
-    path::{Path, PathBuf},
+    path::Path,
     process::Command,
 };
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(global = true, long, default_value = "maskfile.md")]
+    /// Path to a different maskfile you want to use
+    maskfile: String,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Runs the linters.
+    Run {},
+    /// Extracts all the commands from the maskfile and dumps them as files
+    /// into the defined directory.
+    Dump {
+        #[arg(short, long)]
+        output: String,
+    },
+}
+
 fn main() -> Result<()> {
-    let content = fs::read_to_string("maskfile.md")?;
+    let cli = Cli::parse();
+
+    let content = fs::read_to_string(cli.maskfile)?;
     let maskfile = mask_parser::parse(content);
-    // let tmp_dir = tempfile::tempdir()?;
-    let tmp_dir: PathBuf = "./test".parse()?;
-    fs::remove_dir_all(&tmp_dir)?;
-    fs::create_dir_all(&tmp_dir)?;
+
+    // keeping it here to not let it go out of scope
+    // TODO: refactor to only create tempdir if needed
+    let tmp_dir = tempfile::tempdir()?;
+    let mut out_dir = tmp_dir.path().to_path_buf();
+    if let Commands::Dump { output } = &cli.command {
+        drop(tmp_dir);
+        out_dir = output.parse()?;
+        fs::create_dir_all(&out_dir)?;
+    }
 
     for command in maskfile.commands {
         let script = match command.script {
@@ -29,14 +60,17 @@ fn main() -> Result<()> {
 
         let mut file_name = command.name.clone();
         file_name.push_str(linter.file_extension());
-        let file_path = tmp_dir.join(&file_name);
+        let file_path = out_dir.join(&file_name);
         let mut script_file = File::options().create_new(true).append(true).open(&file_path)?;
         let content = linter.content(&script)?;
-        script_file.write(content.as_bytes())?;
+        script_file.write_all(content.as_bytes())?;
 
+        if matches!(cli.command, Commands::Dump { .. }) {
+            continue;
+        }
         println!("checking {}", &command.name);
         let findings = linter.execute(&file_path)?;
-        println!("{}", findings);
+        println!("{findings}");
     }
     Ok(())
 }
